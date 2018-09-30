e25a8830496e61c08e475baf1121423830810057
Qudenaza <qudenaza@gmail.com>
2018-09-30 09:30:32 +0300

    Исправления для тач событий

--- js/scripts.js
+++ js/scripts.js
@@ -7,17 +7,20 @@ class Product {
     this.annotation = father.nextElementSibling;
   }
 
-  changeProductCondition(e, isSelected) {
+  changeProductCondition(e, isSelected, clickEvent) {
     this.isSelected = isSelected;
 
     if (this.isSelected) {
-      return e.sourceCapabilities.firesTouchEvents ? this.makeSelected(e) : this.fatherElement.addEventListener('mouseleave', this.makeSelected);
+      return clickEvent === 'click'
+        ? this.fatherElement.addEventListener('mouseleave', this.makeSelected)
+        : this.makeSelected(e);
     }
 
     if (!this.isSelected) {
       this.classList.remove('product--selected');
 
-      this.annotation.firstElementChild.textContent = 'Чего сидишь? Порадуй котэ,';
+      this.annotation.firstElementChild.textContent =
+        'Чего сидишь? Порадуй котэ,';
 
       this.fatherElement.removeEventListener('mouseleave', this.makeSelected);
     }
@@ -26,17 +29,20 @@ class Product {
   changeLinkCondition(e) {
     e.preventDefault();
 
-    this.makeSelected(e)
+    this.makeSelected(e);
   }
 
   makeSelected(e) {
+    e.preventDefault();
     if (e.target.classList.contains('product__link')) {
-      e.target.previousElementSibling.textContent = e.target.parentElement.dataset.text;
+      e.target.previousElementSibling.textContent =
+        e.target.parentElement.dataset.text;
       this.classList.add('product--selected');
       return;
     }
 
-    e.currentTarget.nextElementSibling.firstElementChild.textContent = e.currentTarget.nextElementSibling.dataset.text;
+    e.currentTarget.nextElementSibling.firstElementChild.textContent =
+      e.currentTarget.nextElementSibling.dataset.text;
     this.classList.add('product--selected');
   }
 
@@ -46,38 +52,51 @@ class Product {
     if (this.isDisabled) {
       const ingredient = this.fatherElement.children[2].textContent;
 
-      this.annotation.firstElementChild.textContent = `Печалька, ${ingredient} закончился.`
+      this.annotation.firstElementChild.textContent = `Печалька, ${ingredient} закончился.`;
     }
   }
 }
 
-
-(function () {
-
+(function() {
+  const clickEvent = document.ontouchstart !== null ? 'click' : 'touchstart';
   let products = [];
+  console.log(clickEvent);
 
   document.querySelectorAll('.product__wrapper').forEach(product => {
-    products.push(new Product(false, product, product.classList.contains('product--disabled')));
+    products.push(
+      new Product(
+        false,
+        product,
+        product.classList.contains('product--disabled')
+      )
+    );
 
-    product.addEventListener('click', onProductClickHandler);
+    product.addEventListener(clickEvent, onProductClickHandler);
   });
 
-  document.querySelectorAll('.product__link').forEach(link => link.addEventListener('click', onLinkClickHandler));
+  document
+    .querySelectorAll('.product__link')
+    .forEach(link => link.addEventListener('click', onLinkClickHandler));
 
   function onProductClickHandler(e) {
     products.forEach(product => {
       if (e.currentTarget === product.fatherElement && !product.isDisabled) {
-        e.currentTarget.classList.contains('product--selected') ? product.changeProductCondition(e, false) : product.changeProductCondition(e, true);
+        e.currentTarget.classList.contains('product--selected')
+          ? product.changeProductCondition(e, false)
+          : product.changeProductCondition(e, true, clickEvent);
       }
-    })
+    });
   }
 
   function onLinkClickHandler(e) {
     products.forEach(product => {
-      if (e.target.classList.contains('product__link') && e.target.parentElement.previousElementSibling === product.fatherElement) {
-        product.changeLinkCondition(e)
+      if (
+        e.target.classList.contains('product__link') &&
+        e.target.parentElement.previousElementSibling === product.fatherElement
+      ) {
+        product.changeLinkCondition(e);
       }
-    })
+    });
   }
 
   document.addEventListener('DOMContentLoaded', () => {
@@ -85,8 +104,6 @@ class Product {
       if (product.classList.contains('product--disabled')) {
         product.makeDisabled(true);
       }
-    })
-  })
-
+    });
+  });
 })();
-
